31c50bc178979da71eeb4c9d5e26d74532072646
Tako Metonidze <metonidzetamuna@gmail.com>
2025-03-24 17:50:45 +0400

    Refactor Constants.Screen for Dynamic Screen Metrics

--- AppDesignSystem/Sources/AppDesignSystem/Config/Constants.swift
+++ AppDesignSystem/Sources/AppDesignSystem/Config/Constants.swift
@@ -9,9 +9,19 @@ import UIKit
 
 public struct Constants {
   public struct Screen {
-    static let factor = UIScreen.main.bounds.width / 430.0
-    static let heightFactor = UIScreen.main.bounds.height / 932.0
-    static let height = UIScreen.main.bounds.height
-    static let width = UIScreen.main.bounds.width
+    nonisolated(unsafe) static var factor: CGFloat = 1.0
+    nonisolated(unsafe) static var heightFactor: CGFloat = 1.0
+    nonisolated(unsafe) static var height: CGFloat = 0.0
+    nonisolated(unsafe) static var width: CGFloat = 0.0
+
+    @MainActor
+    public static func initialize() {
+      let bounds = UIScreen.main.bounds
+      factor = bounds.width / 430.0
+      heightFactor = bounds.height / 932.0
+      width = bounds.width
+      height = bounds.height
+    }
   }
 }
+
